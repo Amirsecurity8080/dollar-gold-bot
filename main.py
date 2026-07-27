9f2d6794b5404eb0aa15b0875fbdeb8816b2e72a
emir <141667231+Amirsecurity8080@users.noreply.github.com>
2026-07-27 23:56:27 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import requests
 
+BOT_TOKEN = "8985690731:AAGbeHG5hHxjpKoSGBrfCGtN35jGQgCI9as"
+CHAT_ID = "-1004458187017"
+
+message = """📊 تست ربات
+
+✅ اگر این پیام را می‌بینی یعنی ربات درست کار می‌کند.
+"""
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+requests.post(url, data={
+    "chat_id": CHAT_ID,
+    "text": message
+})
